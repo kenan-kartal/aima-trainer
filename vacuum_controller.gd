b1d509b2bba6a1099bd1b6204243e3bb5d6840e1
Kenan Kartal <contact@kenankartal.name>
2025-03-15 18:10:22 +0300

    Add table driven agent

--- vacuum_controller.gd
+++ vacuum_controller.gd
@@ -16,6 +16,128 @@ var guessed_room_position: String = "left"
 var cleaned_current: bool = false
 ###
 
+### Table Driven Agent
+class Percept:
+	var room_position: String
+	var room_is_dirty: bool
+
+func get_percept(pos, is_dirty) -> Percept:
+	var percept = Percept.new()
+	percept.room_position = pos
+	percept.room_is_dirty = is_dirty
+	return percept
+
+var percept_sequence: Array[Percept] = []
+var percept_sequence_max_size: int = 3
+var table = {
+	[get_percept("left", false)]: "right",
+	[get_percept("left", false), get_percept("left", false)]: "right",
+	[get_percept("left", false), get_percept("left", false), get_percept("left", false)]: "right",
+	[get_percept("left", false), get_percept("left", false), get_percept("left", true)]: "suck",
+	[get_percept("left", false), get_percept("left", false), get_percept("right", false)]: "wait",
+	[get_percept("left", false), get_percept("left", false), get_percept("right", true)]: "suck",
+	[get_percept("left", false), get_percept("left", true)]: "suck",
+	[get_percept("left", false), get_percept("left", true), get_percept("left", false)]: "right",
+	[get_percept("left", false), get_percept("left", true), get_percept("left", true)]: "suck",
+	[get_percept("left", false), get_percept("left", true), get_percept("right", false)]: "left",
+	[get_percept("left", false), get_percept("left", true), get_percept("right", true)]: "suck",
+	[get_percept("left", false), get_percept("right", false)]: "wait",
+	[get_percept("left", false), get_percept("right", false), get_percept("left", false)]: "wait",
+	[get_percept("left", false), get_percept("right", false), get_percept("left", true)]: "suck",
+	[get_percept("left", false), get_percept("right", false), get_percept("right", false)]: "left",
+	[get_percept("left", false), get_percept("right", false), get_percept("right", true)]: "suck",
+	[get_percept("left", false), get_percept("right", true)]: "suck",
+	[get_percept("left", false), get_percept("right", true), get_percept("left", false)]: "right",
+	[get_percept("left", false), get_percept("right", true), get_percept("left", true)]: "suck",
+	[get_percept("left", false), get_percept("right", true), get_percept("right", false)]: "wait",
+	[get_percept("left", false), get_percept("right", true), get_percept("right", true)]: "suck",
+	[get_percept("left", true)]: "suck",
+	[get_percept("left", true), get_percept("left", false)]: "wait",
+	[get_percept("left", true), get_percept("left", false), get_percept("left", false)]: "right",
+	[get_percept("left", true), get_percept("left", false), get_percept("left", true)]: "suck",
+	[get_percept("left", true), get_percept("left", false), get_percept("right", false)]: "wait",
+	[get_percept("left", true), get_percept("left", false), get_percept("right", true)]: "suck",
+	[get_percept("left", true), get_percept("left", true)]: "suck",
+	[get_percept("left", true), get_percept("left", true), get_percept("left", false)]: "wait",
+	[get_percept("left", true), get_percept("left", true), get_percept("left", true)]: "suck",
+	[get_percept("left", true), get_percept("left", true), get_percept("right", false)]: "left",
+	[get_percept("left", true), get_percept("left", true), get_percept("right", true)]: "suck",
+	[get_percept("left", true), get_percept("right", false)]: "left",
+	[get_percept("left", true), get_percept("right", false), get_percept("left", false)]: "wait",
+	[get_percept("left", true), get_percept("right", false), get_percept("left", true)]: "suck",
+	[get_percept("left", true), get_percept("right", false), get_percept("right", false)]: "left",
+	[get_percept("left", true), get_percept("right", false), get_percept("right", true)]: "suck",
+	[get_percept("left", true), get_percept("right", true)]: "suck",
+	[get_percept("left", true), get_percept("right", true), get_percept("left", false)]: "right",
+	[get_percept("left", true), get_percept("right", true), get_percept("left", true)]: "suck",
+	[get_percept("left", true), get_percept("right", true), get_percept("right", false)]: "left",
+	[get_percept("left", true), get_percept("right", true), get_percept("right", true)]: "suck",
+	[get_percept("right", false)]: "left",
+	[get_percept("right", false), get_percept("left", false)]: "wait",
+	[get_percept("right", false), get_percept("left", false), get_percept("left", false)]: "right",
+	[get_percept("right", false), get_percept("left", false), get_percept("left", true)]: "suck",
+	[get_percept("right", false), get_percept("left", false), get_percept("right", false)]: "wait",
+	[get_percept("right", false), get_percept("left", false), get_percept("right", true)]: "suck",
+	[get_percept("right", false), get_percept("left", true)]: "suck",
+	[get_percept("right", false), get_percept("left", true), get_percept("left", false)]: "wait",
+	[get_percept("right", false), get_percept("left", true), get_percept("left", true)]: "suck",
+	[get_percept("right", false), get_percept("left", true), get_percept("right", false)]: "left",
+	[get_percept("right", false), get_percept("left", true), get_percept("right", true)]: "suck",
+	[get_percept("right", false), get_percept("right", false)]: "wait",
+	[get_percept("right", false), get_percept("right", false), get_percept("left", false)]: "right",
+	[get_percept("right", false), get_percept("right", false), get_percept("left", true)]: "suck",
+	[get_percept("right", false), get_percept("right", false), get_percept("right", false)]: "left",
+	[get_percept("right", false), get_percept("right", false), get_percept("right", true)]: "suck",
+	[get_percept("right", false), get_percept("right", true)]: "suck",
+	[get_percept("right", false), get_percept("right", true), get_percept("left", false)]: "right",
+	[get_percept("right", false), get_percept("right", true), get_percept("left", true)]: "suck",
+	[get_percept("right", false), get_percept("right", true), get_percept("right", false)]: "left",
+	[get_percept("right", false), get_percept("right", true), get_percept("right", true)]: "suck",
+	[get_percept("right", true)]: "suck",
+	[get_percept("right", true), get_percept("left", false)]: "wait",
+	[get_percept("right", true), get_percept("left", false), get_percept("left", false)]: "right",
+	[get_percept("right", true), get_percept("left", false), get_percept("left", true)]: "suck",
+	[get_percept("right", true), get_percept("left", false), get_percept("right", false)]: "wait",
+	[get_percept("right", true), get_percept("left", false), get_percept("right", true)]: "suck",
+	[get_percept("right", true), get_percept("left", true)]: "suck",
+	[get_percept("right", true), get_percept("left", true), get_percept("left", false)]: "right",
+	[get_percept("right", true), get_percept("left", true), get_percept("left", true)]: "suck",
+	[get_percept("right", true), get_percept("left", true), get_percept("right", false)]: "left",
+	[get_percept("right", true), get_percept("left", true), get_percept("right", true)]: "suck",
+	[get_percept("right", true), get_percept("right", false)]: "wait",
+	[get_percept("right", true), get_percept("right", false), get_percept("left", false)]: "wait",
+	[get_percept("right", true), get_percept("right", false), get_percept("left", true)]: "suck",
+	[get_percept("right", true), get_percept("right", false), get_percept("right", false)]: "left",
+	[get_percept("right", true), get_percept("right", false), get_percept("right", true)]: "suck",
+	[get_percept("right", true), get_percept("right", true)]: "suck",
+	[get_percept("right", true), get_percept("right", true), get_percept("left", false)]: "right",
+	[get_percept("right", true), get_percept("right", true), get_percept("left", true)]: "suck",
+	[get_percept("right", true), get_percept("right", true), get_percept("right", false)]: "wait",
+	[get_percept("right", true), get_percept("right", true), get_percept("right", true)]: "suck",
+}
+###
+
+func add_to_percept_sequence(percept: Percept):
+	percept_sequence.append(percept)
+	while (percept_sequence.size() > percept_sequence_max_size):
+		percept_sequence.remove_at(0)
+
+# returns "left", "right", "suck" or "wait"
+func get_table_action() -> String:
+	for sequence in table:
+		if (percept_sequence.size() != sequence.size()):
+			continue
+		var sequences_match = true
+		for i in percept_sequence.size():
+			if percept_sequence[i].room_position != sequence[i].room_position \
+				|| percept_sequence[i].room_is_dirty != sequence[i].room_is_dirty:
+				sequences_match = false
+				continue
+		if sequences_match:
+			return table[sequence]
+	return "wait"
+###
+
 func reset():
 	guessed_room_position = "left"
 	cleaned_current = false
@@ -34,7 +156,8 @@ func perceive():
 
 func agent_program():
 	#blind_agent()
-	reflex_agent()
+	#reflex_agent()
+	table_driven_agent()
 
 func blind_agent():
 	if (cleaned_current):
@@ -57,3 +180,16 @@ func reflex_agent():
 		move_right()
 	else:
 		move_left()
+
+func table_driven_agent():
+	perceive()
+	add_to_percept_sequence(get_percept(room_position, room_is_dirty))
+	match get_table_action():
+		"left":
+			move_left()
+		"right":
+			move_right()
+		"suck":
+			suck()
+		_:
+			return
